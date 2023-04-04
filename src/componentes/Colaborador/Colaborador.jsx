7ea13cebaf2c25c5c36550fe4a411d90ecdb2286
mteusbarbosa <mateus.bm13@gmail.com>
2023-04-04 15:28:50 -0300

    react icons

--- src/componentes/Colaborador/Colaborador.jsx
+++ src/componentes/Colaborador/Colaborador.jsx
@@ -1,11 +1,10 @@
 import "./colaborador.css";
+import { AiFillCloseCircle } from 'react-icons/ai';
 
 export default function Colaborador({ colaborador, corDeFundo, aoDeletar }) {
   return (
     <div className="colaborador">
-      <div className="deletar" onClick={aoDeletar}>
-        x
-      </div>
+      <AiFillCloseCircle size={25} className="deletar" onClick={aoDeletar}/>
       <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
         <img src={colaborador.imagem} alt={colaborador.nome} />
       </div>
