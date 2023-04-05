3cd56700b95f2db74e9728950729c43b918de40c
mteusbarbosa <mateus.bm13@gmail.com>
2023-04-04 22:20:25 -0300

    refatoracao do componente campo

---
+++ src/componentes/Campo/Campo.jsx
@@ -0,0 +1,23 @@
+import "./campo.css";
+
+export default function Campo({
+  type = "text",
+  label,
+  placeholder,
+  valor,
+  aoAlterado,
+  obrigatorio = false,
+}) {
+  return (
+    <div className={`campo campo-${type}` }>
+      <label>{label}</label>
+      <input
+        type={type}
+        value={valor}
+        onChange={(evento) => aoAlterado(evento.target.value)}
+        required={obrigatorio}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+}
